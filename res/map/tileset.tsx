02fcac32fd65d908ecaf86ca21cbc6108b6c67bf
MadeInShineA <olivier.amacker@netplus.ch>
2024-06-04 16:00:45 +0200

    Updated the map

--- res/map/tileset.tsx
+++ res/map/tileset.tsx
@@ -1,24 +1,29 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="tileset" tilewidth="32" tileheight="32" spacing="1" margin="1" tilecount="400" columns="20">
-    <image source="32x32_map_tile v1.2.png" width="661" height="661"/>
-    <tile id="10">
-    <properties>
-    <property name="tileType" value="bridge"/>
-    </properties>
-</tile>
-<tile id="150">
- <properties>
-  <property name="tileType" value="water"/>
- </properties>
-</tile>
-<tile id="205">
- <properties>
-  <property name="tileType" value="p2_tower"/>
- </properties>
-</tile>
-<tile id="206">
- <properties>
-  <property name="tileType" value="p1_tower"/>
- </properties>
-</tile>
+ <image source="32x32_map_tile v1.2.png" width="661" height="661"/>
+ <tile id="10">
+  <properties>
+   <property name="walkable" value="true"/>
+  </properties>
+ </tile>
+ <tile id="21">
+  <properties>
+   <property name="walkable" value="true"/>
+  </properties>
+ </tile>
+ <tile id="150">
+  <properties>
+   <property name="walkable" value="no"/>
+  </properties>
+ </tile>
+ <tile id="205">
+  <properties>
+   <property name="walkable" value="false"/>
+  </properties>
+ </tile>
+ <tile id="206">
+  <properties>
+   <property name="walkable" value="false"/>
+  </properties>
+ </tile>
 </tileset>
